b2148b2bd7dbf1ce0940b1f5f1f3bfc6efd68810
Marek <marek.lausman@gmail.com>
2020-04-20 13:56:23 +0200

    react build

---
+++ src/main/resources/static/precache-manifest.567881be6c16166f9b06c2886abbe12f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5124cfbd93a7e9bf7939cd3d7f907c46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da87b60bd20f13229887",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "49e74bec54a9a3d55427",
+    "url": "/static/css/main.a7068dd2.chunk.css"
+  },
+  {
+    "revision": "da87b60bd20f13229887",
+    "url": "/static/js/2.c3031008.chunk.js"
+  },
+  {
+    "revision": "aaf3dddb317c6c25b54eb793c6ba63c4",
+    "url": "/static/js/2.c3031008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e74bec54a9a3d55427",
+    "url": "/static/js/main.81348b31.chunk.js"
+  },
+  {
+    "revision": "c1b2f5c48d92ba9f942b",
+    "url": "/static/js/runtime-main.50a9b320.js"
+  },
+  {
+    "revision": "a14bbbbb61088e1b5bc5fcb995f6816e",
+    "url": "/static/media/cina.a14bbbbb.jpg"
+  },
+  {
+    "revision": "0e1c8a48afb73886d9f44ec710ec7153",
+    "url": "/static/media/male.0e1c8a48.png"
+  }
+]);
