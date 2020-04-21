2a02861e24dc3abf094289e3c7be3ac7f711866a
Marek <marek.lausman@gmail.com>
2020-04-21 20:48:43 +0200

    tripgroup issue working?

--- src/main/java/cz/uhk/fim/planapp/domain/TripGroup.java
+++ src/main/java/cz/uhk/fim/planapp/domain/TripGroup.java
@@ -38,8 +38,11 @@ public class TripGroup {
     @JsonIgnore
     private Set<User> users = new HashSet<>();
 
-    @OneToMany(targetEntity=Trip.class,cascade = CascadeType.ALL,
+    /*@OneToMany(targetEntity=Trip.class,cascade = CascadeType.REMOVE,
             fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Trip> trips = new ArrayList<>();*/
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "tripGroup", orphanRemoval = true)
+    @JsonIgnore
     private List<Trip> trips = new ArrayList<>();
 
     public Set<User> getUsers() {
